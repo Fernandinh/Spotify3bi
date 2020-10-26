9fbd211829c5528b229e870efbf12d90607d43ae
Fernando Luiz <64204559+Fernandinh@users.noreply.github.com>
2020-10-26 11:52:57 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/spotify3bi_giulia/MainActivity.java
+++ app/src/main/java/com/example/spotify3bi_giulia/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         img.findViewById(R.id.img_musica);
-        Glide.with(this).load("https://img.youtube.com/vi/CIFrr1dqj5M/0.jpg").into(img);
+        Glide.with(this).load("https://img.youtube.com/vi/hpefredTUA8/0.jpg").into(img);
 
         tocador = MediaPlayer.create(getApplicationContext(), R.raw.house);
     }
